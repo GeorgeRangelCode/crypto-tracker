dce02e842ec12bd654c47dfc97efe0b78efdd54c
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-08-05 17:08:56 -0500

    Navegación entre pantallas

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Pressable, StyleSheet } from "react-native";
 
-const CoinsScreen = () => {
+const CoinsScreen = ({ navigation }) => {
+  const handlePress = () => {
+    console.log("object", navigation);
+    navigation.navigate("CoinDetail");
+  };
+
   return (
-    <View>
-      <Text>Coins Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Coins Screen</Text>
+      <Pressable style={styles.btn} onPress={handlePress}>
+        <Text style={styles.btnText}>Ir a detail</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  titleText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  btn: {
+    padding: 8,
+    backgroundColor: "blue",
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
 
 export default CoinsScreen;
