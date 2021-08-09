f201aceb863782e55f7b1c311e615ff7e6284e0c
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-08-09 16:34:53 -0500

    Lista de favoritos

--- src/components/fovorites/FavoritesScreen.js
+++ src/components/fovorites/FavoritesScreen.js
@@ -1,12 +1,48 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, FlatList, StyleSheet } from "react-native";
 import colors from "../../res/colors";
 import FavoritesEmptyState from "./FavoritesEmptyState";
+import CoinsItem from "../coins/CoinsItem";
+import Storage from "../../libs/storage";
 
-const FavoritesScreen = () => {
+const FavoritesScreen = ({ navigation }) => {
+  const [favorites, setIsFavorites] = useState([]);
+
+  const getFavorites = async () => {
+    try {
+      const allKeys = await Storage.instance.getAllKeys();
+      const keys = allKeys.filter(key => key.includes("favorite-"));
+
+      const favs = await Storage.instance.multiGet(keys);
+
+      const favorites = favs.map(fav => JSON.parse(fav[1]));
+      setIsFavorites(favorites);
+    } catch (error) {
+      console.log("get favorites error ", error);
+    }
+  };
+
+  const handlePress = coin => {
+    navigation.navigate("CoinDetail", { coin });
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getFavorites();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
-      <FavoritesEmptyState />
+      {favorites.length === 0 ? (
+        <FavoritesEmptyState />
+      ) : (
+        <FlatList
+          data={favorites}
+          renderItem={({ item }) => <CoinsItem item={item} onPress={() => handlePress(item)} />}
+        />
+      )}
     </View>
   );
 };
