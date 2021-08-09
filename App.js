faee709195d57c7f79ae71db368714dbef654c50
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-08-09 10:59:42 -0500

    Agregando tabs de navegación

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import CoinsStack from "./src/components/coins/CoinsStack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import colors from "./src/res/colors";
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: "#fefefe",
+          style: {
+            backgroundColor: colors.blackPearl,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarLabel: "Coins",
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require("./src/assets/bank.png")}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
